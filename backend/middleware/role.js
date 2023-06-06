f5927301ca87c4bf6aa44199dbae57329a3fd80f
Sushil787 <itsushil100@outlook.com>
2023-06-06 21:53:09 +0545

    added: seeder

--- backend/middleware/role.js
+++ backend/middleware/role.js
@@ -1,10 +1,10 @@
-const role_check = async(req, res, next)=>{
-    admin_role = req.is_admin;
-    if(admin_role){
-        next();
-        return;
-    }
-   return res.status(401).json({message: "un-authorized"});
+// const role_check = async(req, res, next)=>{
+//     admin_role = req.is_admin;
+//     if(admin_role){
+//         next();
+//         return;
+//     }
+//    return res.status(401).json({message: "un-authorized"});
 
-}
-module.exports = role_check;
+// }
+// module.exports = role_check;
